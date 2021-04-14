8a46dd5de800b403d1f4e1340f0d898cb24a3131
Anastasia <eampe@mail.ru>
2021-04-14 11:19:56 +0300

    first task (012)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-console.log(s);
-var s;
-console.log(s);
+'use  strict';
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const nameMovie = prompt('Один из последних просмотренных фильмов?', '');
+const mark = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[nameMovie] = mark;
+console.log(personalMovieDB);
